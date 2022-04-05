a63a5b896c35dda22c6b04a3b28f82d3a24cc96c
tylershanks <tshanks530@gmail.com>
2022-04-05 16:39:53 -0400

    initial cleanup stuff and inital setup of some minor css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
 
+// eslint-disable-next-line require-jsdoc
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Memory Game</header>
+      <div className='mainContainer'>
+        <div className='currentScore'>00</div>
+        <div className='highScore'>99</div>
+
+        <div className='blockContainer'>
+          <div className='redBlock'></div>
+          <div className='orangeBlock'></div>
+          <div className='yellowBlock'></div>
+          <div className='greenBlock'></div>
+          <div className='blueBlock'></div>
+          <div className='purpleBlock'></div>
+          <div className='blackBlock'></div>
+          <div className='whiteBlock'></div>
+        </div>
+      </div>
     </div>
   );
 }
